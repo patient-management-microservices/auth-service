b7b85aa8af8b64876e71fa4eafe146776b458461
DilshanJay <sdjayawardana18@gmail.com>
2026-07-19 22:42:42 +0530

    add initial configuration and user model for authentication service

---
+++ src/main/java/com/pm/authservice/model/User.java
@@ -0,0 +1,28 @@
+package com.pm.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
